3335d7b00a3432b378d033bca89a884f5c7c20d3
krr <ab8@gmail.com>
2024-08-05 13:51:27 +0530

    first change made in program.cs

--- ConsoleAppGit/Program.cs
+++ ConsoleAppGit/Program.cs
@@ -1,3 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
 
+class Program 
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("First Commit");
+    }
+}
